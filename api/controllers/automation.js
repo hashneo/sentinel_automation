cc60260d575cac18bb2c957470200569a0c90a5f
Steven Taylor <steven.taylor@me.com>
2019-04-25 19:51:58 -0700

    fix for save

--- api/controllers/automation.js
+++ api/controllers/automation.js
@@ -27,7 +27,10 @@ module.exports.postAutomation = (req, res) => {
 
     global.module.getDeviceStatus(js.device)
         .then( (status) =>{
-            return global.module.runInSandbox(js, status || {}, test);
+            if (!test)
+                return Promise.resolve({});
+            else
+                return global.module.runInSandbox(js, status || {}, test);
         })
         .then( (result) => {
 
